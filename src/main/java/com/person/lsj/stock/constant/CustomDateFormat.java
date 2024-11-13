ffe04d2d526c3a8377879c1c47fb5dd09be312ee
q310208626 <15521385907@163.com>
2024-11-13 22:25:29 +0800

    change result search:add history search function

--- src/main/java/com/person/lsj/stock/constant/CustomDateFormat.java
+++ src/main/java/com/person/lsj/stock/constant/CustomDateFormat.java
@@ -2,6 +2,7 @@ package com.person.lsj.stock.constant;
 
 public class CustomDateFormat {
     public static final String DATE_FORMAT = "yyyy-MM-dd";
+    public static final String DATE_FORMAT_TIGHT = "yyyyMMdd";
     public static final String TIME_FORMAT = "HH:mm:ss";
     public static final String DATE_TIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
 }
--- src/main/java/com/person/lsj/stock/controller/StockDataResultController.java
+++ src/main/java/com/person/lsj/stock/controller/StockDataResultController.java
@@ -2,14 +2,14 @@ package com.person.lsj.stock.controller;
 
 import com.person.lsj.stock.bean.dongfang.result.StockDataResultDetails;
 import com.person.lsj.stock.bean.dongfang.result.StockDataResultSum;
+import com.person.lsj.stock.constant.CustomDateFormat;
 import com.person.lsj.stock.service.StockDataCapturerService;
 import com.person.lsj.stock.service.StockDataResultService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
@@ -24,10 +24,11 @@ public class StockDataResultController {
     @Autowired
     private StockDataCapturerService stockDataCapturerService;
 
-    @GetMapping("/lastday")
-    public List<StockDataResultSum> getYestedayData() {
-        LocalDate lastWorkDay = stockDataResultService.getLatestWorkDay();
-        List<StockDataResultSum> stockDataResultSumList = stockDataResultService.queryStockDataResult(lastWorkDay);
+    @GetMapping("/{searchDate}")
+    public List<StockDataResultSum> getYestedayData(@PathVariable String searchDate) {
+        System.out.println(searchDate);
+        LocalDate collectDate = LocalDate.parse(searchDate, DateTimeFormatter.ofPattern(CustomDateFormat.DATE_FORMAT_TIGHT));
+        List<StockDataResultSum> stockDataResultSumList = stockDataResultService.queryStockDataResult(collectDate);
         stockDataResultSumList.sort(new Comparator<StockDataResultSum>() {
             @Override
             public int compare(StockDataResultSum o1, StockDataResultSum o2) {
@@ -47,36 +48,6 @@ public class StockDataResultController {
             }
 
             stockDataResultSum.setStockDataResultDetailsList(stockDataResultDetailsList);
-        }
-
-        return stockDataResultSumList;
-    }
-
-    @GetMapping("/secondToLastday")
-    public List<StockDataResultSum> getSecondToLastDayData() {
-        LocalDate secondToLastDay = stockDataResultService.getSecondToLastDay();
-        if (secondToLastDay == null) {
-            return List.of();
-        }
-
-        List<StockDataResultSum> stockDataResultSumList = stockDataResultService.queryStockDataResult(secondToLastDay);
-        stockDataResultSumList.sort(new Comparator<StockDataResultSum>() {
-            @Override
-            public int compare(StockDataResultSum o1, StockDataResultSum o2) {
-                return o1.getTaskId().compareTo(o2.getTaskId());
-            }
-        });
-
-        for (StockDataResultSum stockDataResultSum : stockDataResultSumList) {
-            List<StockDataResultDetails> stockDataResultDetailsList = stockDataResultSum.getStockDataResultDetailsList();
-            List<String> stockCodeList = stockDataResultDetailsList.stream().map(x -> x.getStockCode()).collect(Collectors.toList());
-            Map<String, String> stockCodesNames = stockDataCapturerService.getStockCodesNames(stockCodeList);
-
-            for (StockDataResultDetails stockDataResultDetails : stockDataResultDetailsList) {
-                String stockCode = stockDataResultDetails.getStockCode();
-                String stockName = stockCodesNames.get(stockCode);
-                stockDataResultDetails.setStockName(stockName);
-            }
         }
 
         return stockDataResultSumList;
