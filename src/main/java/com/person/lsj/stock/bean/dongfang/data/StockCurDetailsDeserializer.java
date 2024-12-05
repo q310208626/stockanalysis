746db841c62f0c3bf8909afed4cff831404864b6
q310208626 <15521385907@163.com>
2024-12-05 11:33:45 +0800

    fix deserialize problem

--- src/main/java/com/person/lsj/stock/bean/dongfang/data/StockCurDetailsDeserializer.java
+++ src/main/java/com/person/lsj/stock/bean/dongfang/data/StockCurDetailsDeserializer.java
@@ -5,11 +5,13 @@ import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.JsonNode;
+import org.apache.log4j.Logger;
 
 import java.io.IOException;
 import java.text.DecimalFormat;
 
 public class StockCurDetailsDeserializer extends JsonDeserializer<StockCurDetailsData> {
+    private static Logger LOGGER = Logger.getLogger(StockCurDetailsDeserializer.class);
     private static final String CUR_PRICE = "f43";
     private static final String HIGH_PRICE = "f44";
     private static final String LOW_PRICE = "f45";
@@ -23,7 +25,7 @@ public class StockCurDetailsDeserializer extends JsonDeserializer<StockCurDetail
     private DecimalFormat df = new DecimalFormat(".00");
 
     @Override
-    public StockCurDetailsData deserialize(JsonParser jsonParser, DeserializationContext ctxt) throws IOException, JacksonException {
+    public StockCurDetailsData deserialize(JsonParser jsonParser, DeserializationContext ctxt) throws IOException, JacksonException{
         StockCurDetailsData stockCurDetailsData = new StockCurDetailsData();
 
         JsonNode jsonNode = jsonParser.getCodec().readTree(jsonParser);
@@ -45,33 +47,57 @@ public class StockCurDetailsDeserializer extends JsonDeserializer<StockCurDetail
         }
 
         if (dataNode.get(CUR_PRICE) != null) {
-            String curPrice = dataNode.get(CUR_PRICE).asText();
-            stockCurDetailsData.setCur(df.format(Float.valueOf(curPrice) / 100));
+            try {
+                String curPrice = dataNode.get(CUR_PRICE).asText();
+                stockCurDetailsData.setCur(df.format(Float.valueOf(curPrice) / 100));
+            } catch (NumberFormatException e) {
+                stockCurDetailsData.setCur("0");
+            }
         }
 
         if (dataNode.get(HIGH_PRICE) != null) {
-            String highPrice = dataNode.get(HIGH_PRICE).asText();
-            stockCurDetailsData.setHigh(df.format(Float.valueOf(highPrice) / 100));
+            try {
+                String highPrice = dataNode.get(HIGH_PRICE).asText();
+                stockCurDetailsData.setHigh(df.format(Float.valueOf(highPrice) / 100));
+            } catch (NumberFormatException e) {
+                stockCurDetailsData.setHigh("0");
+            }
         }
 
         if (dataNode.get(LOW_PRICE) != null) {
-            String lowPrice = dataNode.get(LOW_PRICE).asText();
-            stockCurDetailsData.setLow(df.format(Float.valueOf(lowPrice) / 100));
+            try {
+                String lowPrice = dataNode.get(LOW_PRICE).asText();
+                stockCurDetailsData.setLow(df.format(Float.valueOf(lowPrice) / 100));
+            } catch (NumberFormatException e) {
+                stockCurDetailsData.setLow("0");
+            }
         }
 
         if (dataNode.get(OPEN_PRICE) != null) {
-            String openPrice = dataNode.get(OPEN_PRICE).asText();
-            stockCurDetailsData.setOpen(df.format(Float.valueOf(openPrice) / 100));
+            try {
+                String openPrice = dataNode.get(OPEN_PRICE).asText();
+                stockCurDetailsData.setOpen(df.format(Float.valueOf(openPrice) / 100));
+            } catch (NumberFormatException e) {
+                stockCurDetailsData.setOpen("0");
+            }
         }
 
         if (dataNode.get(VOLUME) != null) {
-            String volume = dataNode.get(VOLUME).asText();
-            stockCurDetailsData.setVolume(Integer.valueOf(volume));
+            try {
+                String volume = dataNode.get(VOLUME).asText();
+                stockCurDetailsData.setVolume(Integer.valueOf(volume));
+            } catch (NumberFormatException e) {
+                stockCurDetailsData.setVolume(0);
+            }
         }
 
         if (dataNode.get(INCREASE_PERCENTAGE) != null) {
-            String increasePercentage = dataNode.get(INCREASE_PERCENTAGE).asText();
-            stockCurDetailsData.setIncreasePercentage(df.format(Float.valueOf(increasePercentage) / 100));
+            try {
+                String increasePercentage = dataNode.get(INCREASE_PERCENTAGE).asText();
+                stockCurDetailsData.setIncreasePercentage(df.format(Float.valueOf(increasePercentage) / 100));
+            } catch (NumberFormatException e) {
+                stockCurDetailsData.setIncreasePercentage("0");
+            }
         }
 
         if (dataNode.get(STOCK_STATUS) != null) {
